2d7d789e9fad651d7cea8bb59004aa60bef5830a
Patrick McKelvy <pmckelvy1@gmail.com>
2015-12-07 15:42:36 -0500

    add sliding, stepping, and pawn sublcasses

--- chess/chess_pieces.rb
+++ chess/chess_pieces.rb
@@ -1,21 +1,27 @@
 require_relative "chess_board"
 
-class Piece(board)
+class Piece
 
   attr_accessor :board, :postion
 
-  def initialize(position)
+  def initialize(board, is_black, position)
+    @board = board
+    @is_black = is_black
     @position = position
   end
 
   def moves
   end
+
+  def is_black?
+    is_black
+  end
 end
 
 
 
-class SlidingPiece < Piece
 
+class SlidingPiece < Piece
 
   def moves
     possible_moves = []
@@ -26,8 +32,8 @@ class SlidingPiece < Piece
         new_pos = [new_pos[0] + direction[0], new_pos[1] + direction[1]]
       end
       possible_moves << new_pos if board[new_pos].is_opponent? #take the opponent piece
-      end
     end
+    possible_moves
   end
 
 end
@@ -57,11 +63,72 @@ end
 
 
 
+
+
 class SteppingPiece < Piece
+  def moves
+    possible_moves = []
+    self.move_dirs.each do |direction|
+      new_pos = [@position[0] + direction[0], @position[1] + direction[1]]
+      if board.in_bounds(new_pos)
+        possible_moves << new_pos if board[new_pos].nil? || board[new_pos].is_opponent?
+      end
+    end
+    possible_moves
+  end
 
 end
 
 
+class Knight < SteppingPiece
+  def move_dirs
+    [[-2, -1], [-2,  1], [-1, -2], [-1,  2], [ 1, -2], [ 1,  2], [ 2, -1], [ 2,  1]]
+  end
+end
+
+
+class King < SteppingPiece
+  def move_dirs
+    [[-1, 1], [-1, -1], [1, -1], [1, 1], [1, 0], [0, 1], [0, -1], [-1, 0]]
+  end
+end
+
+
+
+
+
+
 class Pawn < Piece
+
+  def moves
+    # if board[new_pos].is_opponent?
+    possible_moves = []
+    #move [0, 1] if in_bounds and nil
+    new_pos = [@position[0] + 0, @position[1] + 1]
+    if board.in_bounds(new_pos)
+      possible_moves << new_pos if board[new_pos].nil?
+    end
+    #if if opponent is at -1, 1, [1, 1] is_opponent?
+    new_pos = [@position[0] + 1, @position[1] + 1]
+    if board[new_pos].is_opponent?
+      possible_moves << new_pos
+    end
+
+    new_pos = [@position[0] + -1, @position[1] + 1]
+    if board[new_pos].is_opponent?
+      possible_moves << new_pos
+    end
+
+    #if starting point - y = 1 or 6 and on board
+    if self.is_black? && @position[1] == 1
+      new_pos = [@position[0], @position[1] + 2]
+      possible_moves << new_pos if board[new_pos].nil?
+    elsif !(self.is_black?) && position[1] == 6
+      new_pos = [@position[0], @position[1] + 2]
+      possible_moves << new_pos if board[new_pos].nil?
+    end
+
+    possible_moves
+  end
 
 end
